bc43ffa89cb26aab1f0d0be23ba005b4e3bdc625
igrief <isaiahgrief@gmail.com>
2020-03-05 10:27:10 -1000

    Setting up files

---
+++ CaptionSearcher.js
@@ -0,0 +1,13 @@
+
+function updateChannel(){
+    alert("Channel updated");
+}
+
+function submitQuote(){
+    alert("Quote submitted");
+}
+
+function searchQuote(){
+    alert("Quote matched");
+}
+
